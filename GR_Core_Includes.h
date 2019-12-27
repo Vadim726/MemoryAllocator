71ea9a0378ab557d819d63c3ab00a005550c1c73
Vadim726 <59296799+Vadim726@users.noreply.github.com>
2019-12-28 04:42:06 +0500

    Add files via upload

---
+++ GR_Core_Includes.h
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created 	: 04.12.2019
+//	Author		: Vadim Stydnansky
+//	Copyright (c) 2019 the GameRay project.
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef GAMERAY_MYMEM_INCLUDES_H_
+#define GAMERAY_MYMEM_INCLUDES_H_
+
+#include "Common/Building.h"	// Macro platform, export ...
+#include "Common/TypeDefs.h"
+
+#endif
---
+++ stdafx.h
@@ -0,0 +1,10 @@
+#ifndef STDAFX_H_INCLUDED
+#define STDAFX_H_INCLUDED
+
+#define GAMERAY_CORE_BUILDING	1
+
+#define GAMERAY_IN_PCH			1
+#include "myMem_Includes.h"
+#undef GAMERAY_IN_PCH
+
+#endif
